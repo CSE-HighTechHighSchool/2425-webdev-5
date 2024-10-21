e88bbacc07193f62e22bbd8c9864a058867c8c8f
Messef <85040583+AkTewari123@users.noreply.github.com>
2024-10-21 12:19:22 -0400

    added higher gradients

--- js/history.js
+++ js/history.js
@@ -1,9 +1,12 @@
 let historyOneBottom = 0;
-let hasRan = false;
 let i = 0;
+let navHistory = document.getElementsByClassName("navHistory");
+navHistory[0].style.borderBottom = "1px solid white";
+navHistory[0].style.fontSize = "18px";
+
 const historyLabelArray = ["The First Race", "Into the Billions"];
 window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("historyTitle").innerHTML = historyLabelArray[0];
+  // document.getElementById("historyTitle").innerHTML = historyLabelArray[0];
   const historyOneRect = document
     .getElementById("historyOne")
     .getBoundingClientRect();
@@ -16,11 +19,26 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 window.addEventListener("scroll", () => {
-  if (window.scrollY + window.innerHeight / 2 > historyOneBottom && !hasRan) {
+  if (window.scrollY + window.innerHeight / 2 > historyOneBottom) {
     console.log("success");
-    hasRan = true;
-    i++;
-    document.getElementById("historyTitle").innerHTML = historyLabelArray[i];
+    i = 1;
+    navHistory[0].style.borderBottom = "0px";
+    navHistory[0].style.fontSize = "14px";
+
+    navHistory[i].style.fontSize = "18px";
+
+    navHistory[i].style.borderBottom = "1px solid white";
+    // alert("success");
+    // document.getElementById("historyTitle").innerHTML = historyLabelArray[i];
+  } else {
+    i = 0;
+    navHistory[1].style.borderBottom = "0px";
+    navHistory[1].style.fontSize = "14px";
+
+    navHistory[i].style.borderBottom = "1px solid white";
+    navHistory[i].style.fontSize = "18px";
+
+    // document.getElementById("historyTitle").innerHTML = historyLabelArray[i];
   }
   //   console.log(document.getElementById("circleOutline"));
   //   } else {
