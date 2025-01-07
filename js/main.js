d076f892167150e22f5b3a94eb14a7f76487d648
Messef <85040583+AkTewari123@users.noreply.github.com>
2025-01-07 12:50:30 -0500

    added it so that the navbar reflects auth

--- js/main.js
+++ js/main.js
@@ -11,6 +11,17 @@ document.getElementById("navbarlogo").addEventListener("mouseleave", (e) => {
 });
 
 window.addEventListener("DOMContentLoaded", () => {
+  const userString = localStorage.getItem("user");
+  const user = userString != null ? JSON.parse(userString) : null;
+  if (user != null) {
+    document.getElementById("signIn").style.display = "none";
+    document.getElementById("register").style.display = "none";
+    document.getElementById("user").style.display = "block";
+    document.getElementById(
+      "user"
+    ).innerHTML = `<b><i class="bi bi-people">${user.firstname}</i></b>`;
+  }
+
   document.getElementById("XMarkOut").addEventListener("click", () => {
     // When you click on the X in the alert, make it invisible by adding it to a class
     document.getElementById("startingAlert").classList.add("invisible");
